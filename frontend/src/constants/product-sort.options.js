4d4be67120e1cab8aac2fbcbdbc495d2e9c0c6b4
Dmytro Didukh <jeriko.outback@gmail.com>
2020-09-14 15:40:31 +0300

    product labels

--- frontend/src/constants/product-sort.options.js
+++ frontend/src/constants/product-sort.options.js
@@ -3,7 +3,7 @@ const productSortObject = {
         sortOptions: [
                 { key: 'priceLow', text: 'Спочатку дешевші', value: 'priceLow' },
                 { key: 'priceHigh', text: 'Спочатку дорожчі', value: 'priceHigh' },
-                { key: 'rating', text: 'Рейтингом', value: 'rating' },
+                { key: 'popular', text: 'Популярні', value: 'rating' },
                 { key: 'new', text: 'Новинки', value: 'new' },
         ],
         sortName: 'Сортувати за'
--- frontend/src/pages/product-list/product-card/index.jsx
+++ frontend/src/pages/product-list/product-card/index.jsx
@@ -1,44 +1,45 @@
 import React, {useEffect, useState} from 'react'
-import {Card, Image, Button} from 'semantic-ui-react';
+import {Card, Image, Button, Label} from 'semantic-ui-react';
 import {Link} from 'react-router-dom'
 
-import {linkGenerator} from "../../../utils";
 import './style.scss';
 
 const ProductCard = ({product}) => {
-    const [rating, setRating] = useState(10);
-    const [productAvailable, setProductAvailable] = useState(true)
 
 
-    useEffect(() => {
+    const labelGenerator = (color, inner) => (
+        <Label color={color} ribbon>
+            {inner}
+        </Label>
+    )
 
-        if (product && product.rating.length > 0) {
-            setRating(product.rating.reduce((a, b) => a + b.value, 0) / product.rating.length);
+    const salePercentage = Math.round(((product.oldPrice-product.price)/product.oldPrice)*100)
 
-            const sizes = Object.values(product.sizes);
-            sizes.pop()
-            setProductAvailable(sizes.some(item => item))
-        }
-    }, [product, productAvailable])
+    if (product.name === 'Mattioli') {
+        console.log('hot',product.hot)
+        console.log('sale',product.sale)
+    }
 
     return (
-        <Link className="ui card product-card" to={{pathname: linkGenerator(product), query: product}}>
-            <Image src={product.images[0].link} wrapped ui={false}/>
+        <Link className="ui card product-card" to={`/catalog/${product.id}`}>
+            <div className={'product-card__label-container'}>
+                {product.newItem && labelGenerator('green', 'НОВИНКА')}
+                {product.hot && labelGenerator('orange', 'ХІТ ПРОДАЖ')}
+                {product.sale && labelGenerator('red', 'РОЗПРОДАЖ')}
+            </div>
+            <Image src={product.images.product[0].link} wrapped ui={false}/>
             <Card.Content>
                 <div className="product-card__name">
                     {product.name}
                 </div>
-
-                <div className="rating-bar">
-                    <div className="rating-bar__text">Рейтинг:</div>
-                    <div className="rating-bar__rate">{`${rating.toFixed(0)}/10`}</div>
-                </div>
             </Card.Content>
             <Card.Content extra>
                 <div className="product-card__price">
-                    {`Ціна: ${product.price} грн.`}
+                    Ціна: {product.sale && <strike>{product.oldPrice} </strike>}
+                    <span> {product.price}</span> UAH
+                    {product.sale && <Label color='red'>-{salePercentage} %</Label>}
                 </div>
-                {!productAvailable && <div>Немає в наявності</div>}
+                {!product.available && <div>Немає в наявності</div>}
             </Card.Content>
         </Link>
     )
